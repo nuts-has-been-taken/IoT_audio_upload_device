750152c3fb72094b20cc18aaf8c6371538f9841e
carbarcha <samhoward13261@gmail.com>
2023-12-20 16:55:30 +0800

    add not mp3 file handle

--- service/upload_audio.py
+++ service/upload_audio.py
@@ -4,6 +4,9 @@ from util.mp3_to_base import mp3_to_base64
 def upload_audio_to_iottalk(audio:_TemporaryFileWrapper):
     
     try:
+        if audio.name[-4:] != "mp3":
+            return f"{audio.name}",f"Only support mp3 file"
+        
         base64_string = mp3_to_base64(audio)
 
         ### 上傳到 iottalk 
